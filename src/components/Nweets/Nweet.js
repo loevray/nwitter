b50987ce16661b49f06cbc180d5298dc10638121
loevray <87127340+loevray@users.noreply.github.com>
2021-09-07 04:07:41 +0900

    bugggg

--- src/components/Nweets/Nweet.js
+++ src/components/Nweets/Nweet.js
@@ -61,18 +61,20 @@ const Nweet = ({ nweetObj, isOwner, profile, userObj }) => {
         setNewNweet(value);
     };
     const onLikeBtnClick = async (event) => {
-        const path = event.target.className.baseVal;
-        const pathRef = dbService.collection("nweets").doc(`${path}`);
-        console.log(path);
-        if(nweetObj.reNweet.includes(authService.currentUser.uid, 0)) {
+        if(event.target.className.baseVal != null){
+            const path = event.target.className.baseVal;
+            const pathRef = dbService.collection("nweets").doc(`${path}`);
+            console.log(path);
+            if(nweetObj.reNweet.includes(authService.currentUser.uid, 0)) {
+                await pathRef.update({
+                like: dbStore.FieldValue.arrayRemove(`${authService.currentUser.uid}`)
+                })
+                return;
+            }
             await pathRef.update({
-            like: dbStore.FieldValue.arrayRemove(`${authService.currentUser.uid}`)
+                like: dbStore.FieldValue.arrayUnion(`${authService.currentUser.uid}`)
             })
-            return;
         }
-        await pathRef.update({
-            like: dbStore.FieldValue.arrayUnion(`${authService.currentUser.uid}`)
-        })
     }
      const onReNweetBtnClick = async (event) => {
         const path = event.target.className.baseVal;
@@ -163,7 +165,7 @@ const Nweet = ({ nweetObj, isOwner, profile, userObj }) => {
                                 <div className="nweet_right_bottom_reNweet">
                                     <svg viewBox="0 0 24 24" 
                                     aria-hidden="true" 
-                                     className={nweetObj.id}
+                                    className={nweetObj.id}
                                     onClick={onReNweetBtnClick}>
                                         <g>
                                             <path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z">
